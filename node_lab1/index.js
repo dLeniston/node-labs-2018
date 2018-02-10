ff836721d3d89c307cb41fd22e1c4eb18609fb94
Darren Leniston <dleniston@yahoo.com>
2018-02-10 15:21:50 +0000

    Added contacts api and index

--- node_lab1/index.js
+++ node_lab1/index.js
@@ -1,27 +1,14 @@
-//load http module to create http server
 import dotenv from 'dotenv';
 import express from 'express';
-import greeting from './greeting';
+import contactsRouter from './api/contacts';
 
 dotenv.config();
-
+const port = process.env.PORT;
 const app = express();
 
-const port = process.env.PORT;
-
+app.use('/api/contacts', contactsRouter);
 app.use(express.static('public'));
 
-//add route for greeting
-app.get('/greeting', (req, res)=>{
-  let lang = req.headers['accept-language'];
-  const defaultLang = 'en';
-  if(!greeting[lang]) lang = defaultLang;
-  const response = {
-    lang: lang,
-    message: greeting[lang]
-  };
-});
-
 app.listen(port, () => {
-  console.info('Server running at '+port);
+  console.info('Express listening on port: ' + port);
 });
